761a0a24f90849474b8a2e20d0b9e073f5f034d2
CelioBarros <celio.barros.filho@gmail.com>
2016-08-12 11:55:24 -0300

    Mapa mostrando capacidade do açude corretamente

--- app/scripts/directives/sab-mapa.js
+++ app/scripts/directives/sab-mapa.js
@@ -13,8 +13,7 @@
         restrict: 'E',
         scope: {
           onSelectReservatorio: '&',
-          reservatorioSelecionado: '=',
-          infoReservatorios: '='
+          reservatorioSelecionado: '='
         },
         link: function postLink(scope, element) {
           var
@@ -59,27 +58,21 @@
           };
 
           var scaleCircle = function(d) {
-                for (var i = 0; i < scope.infoReservatorios.length; i++) {
-                  if(parseInt(d.properties.ID) === parseInt(scope.infoReservatorios[i].id)){
-                  console.log(scope.infoReservatorios[i]);
-                    if (scope.infoReservatorios[i].capacidade <= 10) {
-                      return (0.5);
-                    } else if ((scope.infoReservatorios[i].capacidade > 10) && (scope.infoReservatorios[i].capacidade <= 100)) {
-                      return (1.0);
-                    } else if ((scope.infoReservatorios[i].capacidade > 100) && (scope.infoReservatorios[i].capacidade <= 250)) {
-                      return (1.5);
-                    } else if ((scope.infoReservatorios[i].capacidade > 250) && (scope.infoReservatorios[i].capacidade <= 500)) {
-                      return (2);
-                    } else if ((scope.infoReservatorios[i].capacidade > 500) && (scope.infoReservatorios[i].capacidade <= 750)) {
-                      return (3);
-                    } else if (scope.infoReservatorios[i].capacidade > 750) {
-                      return (4);
-                    } else{
-                      return(10);
-                    }
-                  }
+                if (d.properties.CAPACIDADE <= 10) {
+                  return (0.5);
+                } else if ((d.properties.CAPACIDADE > 10) && (d.properties.CAPACIDADE <= 100)) {
+                  return (1.0);
+                } else if ((d.properties.CAPACIDADE > 100) && (d.properties.CAPACIDADE <= 250)) {
+                  return (1.5);
+                } else if ((d.properties.CAPACIDADE > 250) && (d.properties.CAPACIDADE <= 500)) {
+                  return (2);
+                } else if ((d.properties.CAPACIDADE > 500) && (d.properties.CAPACIDADE <= 750)) {
+                  return (3);
+                } else if (d.properties.CAPACIDADE > 750) {
+                  return (4);
+                } else{
+                  return(10);
                 }
-                //return(2);
             };
 
           function zoomed() {
