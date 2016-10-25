7286c93354b4fa288637fa855ade98dd8e328820
Jefferson Neves <jefferson.rpn@gmail.com>
2016-10-25 09:49:48 -0300

    Atualiza cores da legenda

--- app/scripts/controllers/mapa.js
+++ app/scripts/controllers/mapa.js
@@ -88,9 +88,9 @@
     vm.reservatoriosGeo = [];
     vm.coresReservatorios = [
       {cor: '#ff2222', texto: 'Abaixo de 10%'},
-      {cor: '#ffa42c', texto: '10% - 25%'},
-      {cor: '#ffff31', texto: '25% - 50%'},
-      {cor: '#33ffff', texto: '50% - 75%'},
+      {cor: '#ff8f61', texto: '10% - 25%'},
+      {cor: '#fffc9f', texto: '25% - 50%'},
+      {cor: '#99bfcf', texto: '50% - 75%'},
       {cor: '#3381ff', texto: 'Acima de 75%'},
       {cor: '#ffffff', texto: 'Sem informação'}
     ];
