430d4602b61a72df8cc51aa9f9c156f684cf5bc0
Jefferson Neves <jefferson.rpn@gmail.com>
2016-11-22 15:16:56 -0300

    Exibe pesquisa e legenda em modal para dispositivos pequenos

--- app/scripts/controllers/mapa.js
+++ app/scripts/controllers/mapa.js
@@ -18,6 +18,8 @@
     vm.showInfo = true;
     vm.loadingMap = true;
     vm.loadingInfo = true;
+    vm.showSearchbar = false;
+    vm.showLegend = false;
 
     var larguraTela = $(window).width();
 
@@ -100,6 +102,8 @@
     vm.setSelectedTab = setSelectedTab;
     vm.toggleInfo = toggleInfo;
     vm.hideInfo = hideInfo;
+    vm.toggleSearchbar = toggleSearchbar;
+    vm.toggleLegend = toggleLegend;
 
     vm.reservatorios = Reservatorio.info.query();
 
@@ -111,6 +115,8 @@
     function setReservatorio(reservatorio, lat, lon, zoom) {
       vm.loadingInfo = true;
       vm.showInfo = true;
+      vm.showSearchbar = false;
+      vm.showLegend = false;
       if (!(lat && lon && zoom)) {
         for (var i = 0; i < vm.reservatoriosGeo.length; i++) {
           if (vm.reservatoriosGeo[i].properties.id == reservatorio.id) {
@@ -157,6 +163,14 @@
       vm.showInfo = !vm.showInfo;
     }
 
+    function toggleSearchbar() {
+      vm.showSearchbar = !vm.showSearchbar;
+    }
+
+    function toggleLegend() {
+      vm.showLegend = !vm.showLegend;
+    }
+
     function hideInfo() {
       vm.showInfo = false;
     }
@@ -172,7 +186,7 @@
     }
 
     function corReservatorio(feature) {
-      if(feature.get("volume_percentual") == null){
+      if(feature.get("volume_percentual") == null) {
         return new ol.style.Fill({ color: vm.coresReservatorios[5].cor});
       } else{
         var volume_percentual = parseFloat(feature.get("volume_percentual"));
