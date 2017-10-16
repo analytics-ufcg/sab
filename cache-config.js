ea67d63c787bae53b5934b7dfc8eb9cedb2aa0c1
Jefferson Neves <jefferson.rpn@gmail.com>
2017-10-16 11:35:59 -0300

    Fix configurações de cache

--- cache-config.js
+++ cache-config.js
@@ -9,12 +9,18 @@ module.exports = {
     'dist/**/*.{html,css,js,jpg,jpeg,png,svg,ttf,woff,woff2}',
     'dist/manifest.json'
   ],
-  runtimeCaching: [{
-    urlPattern: /^https:\/\/fonts.(?:googleapis|gstatic).com\/.*/,
-    handler: 'cacheFirst',
-    urlPattern: /^https:\/\/api.insa.gov.br\/.*/,
-    handler: 'networkFirst',
-    urlPattern: /^https:\/\/olhonagua.insa.gov.br\/styles\/fonts\/.*\.(?:ttf|woff)\?.*/,
-    handler: 'cacheFirst'
-  }]
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts.(?:googleapis|gstatic).com\/.*/,
+      handler: 'cacheFirst'
+    },
+    {
+      urlPattern: /^https:\/\/api.insa.gov.br\/.*/,
+      handler: 'networkFirst'
+    },
+    {
+      urlPattern: /^https:\/\/olhonagua.insa.gov.br\/styles\/fonts\/.*\.(?:ttf|woff)\?.*/,
+      handler: 'cacheFirst'
+    }
+  ]
 };
