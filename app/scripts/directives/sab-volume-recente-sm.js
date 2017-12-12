2242bf2834bbc629d11140780edb1ba03d397224
Jefferson Neves <jefferson.rpn@gmail.com>
2017-12-12 18:27:41 -0300

    Atualiza estilo do gráfico de previsão

--- app/scripts/directives/sab-volume-recente-sm.js
+++ app/scripts/directives/sab-volume-recente-sm.js
@@ -14,14 +14,15 @@
         scope: {
           monitoramento: '=',
           previsoes: '=',
-          data: '='
+          data: '=',
+          capacidade: '='
         },
         link: function postLink(scope, element) {
           var
             d3 = $window.d3;
 
             // Set the dimensions of the canvas / graph
-            var margin = {top: 20, right: 30, bottom: 20, left: 30},
+            var margin = {top: 30, right: 30, bottom: 60, left: 30},
                 width = 400 - margin.left - margin.right,
                 height = 200 - margin.top - margin.bottom;
 
@@ -46,23 +47,25 @@
             var x1 = d3.time.scale().range([0, width/2]);
             var x2 = d3.time.scale().range([width/2, width]);
             var y = d3.scale.linear().range([height, 0]);
-            var x1Axis = d3.svg.axis().scale(x1).orient("bottom").tickFormat(localized.timeFormat("%d %b"));
-            var x2Axis = d3.svg.axis().scale(x2).orient("bottom").tickFormat(localized.timeFormat("%d %b"));
+            var x1Axis = d3.svg.axis().scale(x1).orient("bottom").tickFormat(localized.timeFormat("%d %b")).tickSize(5);
+            var x2Axis = d3.svg.axis().scale(x2).orient("bottom").tickFormat(localized.timeFormat("%d %b")).tickSize(15);
+            var x3Axis = d3.svg.axis().scale(x2).orient("bottom").tickFormat(localized.timeFormat("%d %b")).tickSize(25);
+            var yAxis = d3.svg.axis().scale(y).orient("left").ticks(4);
 
             // Define the line
             var valueline1 = d3.svg.line()
                 .interpolate("monotone")
                 .x(function(d) { return x1(d.date); })
-                .y(function(d) { return y(d.volume); });
+                .y(function(d) { return y(d.VolumePercentual); });
             var valueline2 = d3.svg.line()
                 .interpolate("monotone")
                 .x(function(d) { return x2(d.date); })
-                .y(function(d) { return y(d.volume); });
+                .y(function(d) { return y(d.Porcentagem); });
             var valuearea = d3.svg.area()
                 .interpolate("monotone")
                 .x(function(d) { return x1(d.date); })
                 .y0(height)
-                .y1(function(d) { return y(d.volume); });
+                .y1(function(d) { return y(d.VolumePercentual); });
 
             // Adds the svg canvas
             var svgFrame = d3.select(element[0])
@@ -103,26 +106,66 @@
               title1 = gTexts.append('text'),
               title2 = gTexts.append('text'),
               x1AxisSvg = gAxis.append("g")
-                .attr("class", "x axis")
+                .attr("class", "x axis axis-s")
                 .attr("transform", "translate(0," + (height + 1) + ")"),
               x2AxisSvg = gAxis.append("g")
-                .attr("class", "x axis")
+                .attr("class", "x axis axis-2")
                 .attr("transform", "translate(0," + (height + 1) + ")"),
-              // strokeRetirada = gTexts.append('line'),
+              x3AxisSvg = gAxis.append("g")
+                .attr("class", "x axis axis-3")
+                .attr("transform", "translate(0," + (height + 1) + ")"),
+              yAxisSvg = gAxis.append("g")
+                .attr("class", "y axis"),
               circleRetirada = gDetails.append('circle'),
-              // strokeOutorga = gTexts.append('line'),
               circleOutorga = gDetails.append('circle'),
-              textPrevisao = gTexts.append('text');
+              textPrevisao = gTexts.append('text'),
+              xAxisLineSvg = x1AxisSvg.append('line').attr("class", "axis-line"),
+              gLegend = svg.append("g")
+                .attr("class", "legend")
+                .attr("transform", "translate(0," + (height+margin.top) + ")");
+              gLegend.append("text")
+                .attr('x', 15).attr('y', 10)
+                .attr('font-size', '8px')
+                .text("Previsão com base na retirada de água")
+              gLegend.append("text")
+                .attr('x', 15).attr('y', 20)
+                .attr('font-size', '8px')
+                .text("Previsão com base na outorga");
+              gLegend.append("circle")
+                .attr('cx', 10).attr('cy', 8).attr('r', 3)
+                .style({
+                  "fill": "rgb(88, 182, 235)"
+                });
+              gLegend.append("circle")
+                .attr('cx', 10).attr('cy', 18).attr('r', 3)
+                .style({
+                  "fill": "rgb(67, 107, 224)"
+                });
+              gLegend.append("line")
+                .attr('x1', 0).attr('y1', 8)
+                .attr('x2', 10).attr('y2', 8)
+                .style({
+                  "fill": "none",
+                  "stroke-width": "0.8",
+                  "stroke": "rgb(88, 182, 235)"
+                });
+              gLegend.append("line")
+                .attr('x1', 0).attr('y1', 18)
+                .attr('x2', 10).attr('y2', 18)
+                .style({
+                  "fill": "none",
+                  "stroke-width": "0.8",
+                  "stroke": "rgb(67, 107, 224)"
+                });
 
-            scope.$watchCollection(function(scope) { return [scope.monitoramento, scope.previsoes, scope.data]; }, function(newValue) {
+            scope.$watchCollection(function(scope) { return [scope.monitoramento, scope.previsoes, scope.data, scope.capacidade]; }, function(newValue) {
               if ((typeof newValue[0] !== 'undefined') && (newValue[0].volumes.length !== 0) && (typeof newValue[1] !== 'undefined') && (typeof newValue[2] !== 'undefined')) {
-                draw(newValue[0], newValue[1], newValue[2]);
+                draw(newValue[0], newValue[1], newValue[2], newValue[3]);
               }
             });
 
             // Get the data
-            var draw = function(monitoramento, previsoes, data) {
-
+            var draw = function(monitoramento, previsoes, data, capacidade) {
               // Reset drawing
               lineSvg.attr('display', 'none');
               lineRetirada.attr('display', 'none');
@@ -133,14 +176,17 @@
               endCircle.attr('opacity', '0');
               title1.attr('opacity', '0');
               title2.attr('opacity', '0');
-              x1AxisSvg.attr('display', 'none');
-              x2AxisSvg.attr('display', 'none');
-              // strokeRetirada.attr('display', 'none');
+              x1AxisSvg.attr('display', 'none').attr('opacity', 0);
+              x2AxisSvg.attr('display', 'none').attr('opacity', 0);
+              x3AxisSvg.attr('display', 'none').attr('opacity', 0);
+              xAxisLineSvg.attr('display', 'none').attr('opacity', 0);
+              yAxisSvg.attr('display', 'none').attr('opacity', 0);
               circleRetirada.attr('opacity', '0');
-              // strokeOutorga.attr('display', 'none');
               circleOutorga.attr('opacity', '0');
               textPrevisao.attr('display', 'none');
               curtain.attr('width', width);
+              gDetails.attr('opacity', 0);
+              gLegend.attr('opacity', 0);
 
               var
                 volumes = monitoramento.volumes,
@@ -163,17 +209,14 @@
                 d.volume = +d.Volume;
               });
 
-              var allVolumes = [volumes, previsaoRetirada.volumes, previsaoOutorga.volumes];
-              var min = d3.min(allVolumes.map(function(v) {
-                return d3.min(v, function(d) { return d.volume; });
-              }));
-              var max = d3.max(allVolumes.map(function(v) {
-                return d3.max(v, function(d) { return d.volume; });
-              }));
               x2.domain([dataBase, d3.time.day.offset(dataBase, 180)]);
-              y.domain([min, max]);
+              xAxisLineSvg
+                .attr("x1", x2(x2.domain()[0]))
+                .attr("y", height)
+                .attr("x2", x2(x2.domain()[1]));
+              y.domain([0, d3.max(volumes, function(d) { return d.VolumePercentual; })]);
 
-              if (volumes.length) {
+              if (volumes.length > 1) {
                 x1.domain(d3.extent(volumes, function(d) { return d.date; }));
                 x1Axis.tickValues([
                   volumes[0].date,
@@ -211,9 +254,9 @@
                 // Details
                 dayStroke
                   .attr('x1', width * 0.5)
-                  .attr('y1', -margin.top)
+                  .attr('y1', 0)
                   .attr('x2', width * 0.5)
-                  .attr('y2', height + (margin.bottom * 0.5))
+                  .attr('y2', height)
                   .attr('display', 'inline')
                   .style({
                     "stroke": "gray",
@@ -221,7 +264,7 @@
                   });
                 endCircle
                   .attr('cx', width * 0.5)
-                  .attr('cy', y(volumes[volumes.length-1].volume))
+                  .attr('cy', y(volumes[volumes.length-1].VolumePercentual))
                   .attr('r', 3)
                   .attr('display', 'inline')
                   .style({
@@ -242,7 +285,9 @@
                   .attr('text-anchor', 'middle')
                   .text("Previsão");
 
-                x1AxisSvg.call(x1Axis).attr('display', 'inline');
+                x1AxisSvg.call(x1Axis).attr('display', null);
+                xAxisLineSvg.attr('display', null);
+                yAxisSvg.call(yAxis).attr('display', null);
               }
 
             if (previsaoRetirada.volumes.length) {
@@ -255,46 +300,15 @@
                   .attr("class", "retirada")
                   .attr('display', 'inline')
                   .attr("d", valueline2(previsaoRetirada.volumes));
-                // strokeRetirada
-                //   .attr('x1', x2(previsaoRetirada.volumes[previsaoRetirada.volumes.length-1].date))
-                //   .attr('y1', -margin.top)
-                //   .attr('x2', x2(previsaoRetirada.volumes[previsaoRetirada.volumes.length-1].date))
-                //   .attr('y2', height + (margin.bottom * 0.5))
-                //   .attr('display', 'inline')
-                //   .style({
-                //     "stroke": "gray",
-                //     "stroke-width": "0.5",
-                //     "stroke-dasharray": "4,2"
-                //   });
                 circleRetirada
                   .attr('cx', x2(previsaoRetirada.volumes[previsaoRetirada.volumes.length-1].date))
-                  .attr('cy',  y(previsaoRetirada.volumes[previsaoRetirada.volumes.length-1].volume))
+                  .attr('cy',  y(previsaoRetirada.volumes[previsaoRetirada.volumes.length-1].Porcentagem))
                   .attr('r', 3)
                   .style({
                     "fill": "rgb(88, 182, 235)"
                   });
                   x2Axis.tickValues([previsaoRetirada.volumes[previsaoRetirada.volumes.length-1].date]);
                   x2AxisSvg.call(x2Axis).attr('display', 'inline');
-            } else {
-                // strokeRetirada
-                //   .attr('x1', width * 0.5)
-                //   .attr('y1', y(volumes[volumes.length-1].volume))
-                //   .attr('x2', width)
-                //   .attr('y2', height)
-                //   .attr('display', 'inline')
-                //   .style({
-                //     "stroke": "gray",
-                //     "stroke-width": "0.5",
-                //     "stroke-dasharray": "4,2"
-                //   });
-                textPrevisao
-                  .attr('x', width * 0.75)
-                  .attr('y', height * 0.5)
-                  .attr('font-size', '8px')
-                  .attr('text-anchor', 'middle')
-                  .attr('display', 'inline')
-                  .attr('fill', 'gray')
-                  .text("Dados insuficientes");
             }
 
             if (previsaoOutorga.volumes.length) {
@@ -307,30 +321,27 @@
                 .attr("class", "outorga")
                 .attr("d", valueline2(previsaoOutorga.volumes))
                 .attr('display', 'inline');
-              // strokeOutorga
-              //   .attr('x1', x2(previsaoOutorga.volumes[previsaoOutorga.volumes.length-1].date))
-              //   .attr('y1', -margin.top)
-              //   .attr('x2', x2(previsaoOutorga.volumes[previsaoOutorga.volumes.length-1].date))
-              //   .attr('y2', height + (margin.bottom * 0.5))
-              //   .attr('display', 'inline')
-              //   .style({
-              //     "stroke": "gray",
-              //     "stroke-width": "0.5",
-              //     "stroke-dasharray": "4,2"
-              //   });
               circleOutorga
                 .attr('cx', x2(previsaoOutorga.volumes[previsaoOutorga.volumes.length-1].date))
-                .attr('cy',  y(previsaoOutorga.volumes[previsaoOutorga.volumes.length-1].volume))
+                .attr('cy',  y(previsaoOutorga.volumes[previsaoOutorga.volumes.length-1].Porcentagem))
                 .attr('r', 3)
                 .attr('display', 'inline')
                 .style({
                   "fill": "rgb(67, 107, 224)"
                 });
-              x2Axis.tickValues([
-                previsaoRetirada.volumes[previsaoRetirada.volumes.length-1].date,
-                previsaoOutorga.volumes[previsaoOutorga.volumes.length-1].date
-              ]);
-              x2AxisSvg.call(x2Axis).attr('display', 'inline');
+              x3Axis.tickValues([previsaoOutorga.volumes[previsaoOutorga.volumes.length-1].date]);
+              x3AxisSvg.call(x3Axis).attr('display', 'inline');
+            }
+
+            if (!(previsaoRetirada.volumes.length || previsaoOutorga.volumes.length)) {
+                textPrevisao
+                  .attr('x', width * 0.75)
+                  .attr('y', height * 0.5)
+                  .attr('font-size', '8px')
+                  .attr('text-anchor', 'middle')
+                  .attr('display', 'inline')
+                  .attr('fill', 'gray')
+                  .text("Dados insuficientes");
             }
 
             // Animation
@@ -338,17 +349,45 @@
               .delay(500)
               .duration(500)
               .attr('opacity', '1');
+            gDetails.transition()
+              .delay(500)
+              .duration(500)
+              .attr('opacity', '1');
+            gLegend.transition()
+              .delay(500)
+              .duration(500)
+              .attr('opacity', '1');
+            x1AxisSvg.transition()
+              .delay(500)
+              .duration(500)
+              .attr('opacity', '1');
+            xAxisLineSvg.transition()
+              .delay(500)
+              .duration(500)
+              .attr('opacity', '1');
+            yAxisSvg.transition()
+              .delay(500)
+              .duration(500)
+              .attr('opacity', '1');
+            x2AxisSvg.transition()
+              .delay(500)
+              .duration(500)
+              .attr('opacity', '1');
+            x3AxisSvg.transition()
+              .delay(500)
+              .duration(500)
+              .attr('opacity', '1');
             curtain.transition()
               .delay(1000)
-              .duration(1000)
+              .duration(500)
               .ease('linear')
               .attr('width', (width * 0.5))
               .transition()
                 .delay(3000)
-                .duration(1000)
+                .duration(500)
                 .attr('width', 0);
             endCircle.transition()
-              .delay(2000)
+              .delay(1500)
               .duration(200)
               .attr('opacity', '1');
             title2.transition()
@@ -357,13 +396,13 @@
               .attr('opacity', '1');
             if (previsaoRetirada.volumes.length) {
               circleRetirada.transition()
-                .delay(4000)
+                .delay(3200)
                 .duration(200)
                 .attr('opacity', '1');
             }
             if (previsaoOutorga.volumes.length) {
               circleOutorga.transition()
-                .delay(4000)
+                .delay(3200)
                 .duration(200)
                 .attr('opacity', '1');
             }
@@ -385,6 +424,7 @@
             }
 
           };
+
         }
       };
     }
